8dc222e43089fc5d9f2bf79de2d37505936dd0e2
brennash <shane.brennan.ie@gmail.com>
2023-12-28 13:18:10 +0000

    Adding Upload page

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,10 @@ from flask import Flask, request, Response, jsonify
 
 
 database = Database()
-print(os.getcwd())
+
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -20,6 +23,25 @@ def index():
 def print_image():
     image_path = request.args.get('image_path')
     return render_template('print_image.html', image_path=image_path)
+
+@app.route('/upload_image')
+def upload_image():
+    return render_template('upload_image.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return redirect(request.url)
+    
+    if file and allowed_file(file.filename):
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    
+    return redirect(url_for('index'))
 
 @app.route('/search', methods=['GET','POST'])
 def search_image():
@@ -34,13 +56,14 @@ def search_image():
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     print("AUTOCOMPLETE!!")
-    search = request.args.get('q')
+    search = request.args.get('query')
+    print(f"AUTOCOMPLETE SEARCH: {search}")
 
     if not search:
         # If no query is provided, return an empty list
         return jsonify([])
 
-    print(search)
+    print(f"AUTOCOMPLETE {search}")
     results = database.get_autocomplete(search)
     print("RESULTS", results)
     return results
